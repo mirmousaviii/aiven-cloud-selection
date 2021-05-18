ddc617b7ce30d1c80ae2bf98fb93c7b7be84ebca
Mo Mirmousavi <mirmousavi.m@gmail.com>
2021-05-18 07:06:36 +0200

    Add data grid for show clouds

---
+++ src/components/clouds-data-grid/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {DataGrid, GridToolbar} from '@material-ui/data-grid';
+import {makeStyles} from '@material-ui/core/styles';
+import {Box} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    height: '80vw',
+    width: '100%',
+  },
+}));
+
+function CloudsDataGrid({data, isLoading}) {
+  const classes = useStyles();
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      flex: 1,
+      sortable: false,
+      filterable: false,
+      hide: true,
+    },
+    {
+      field: 'cloud_name',
+      headerName: 'Cloud name',
+      width: 300,
+      sortable: true,
+    },
+    {
+      field: 'cloud_description',
+      headerName: 'Cloud description',
+      flex: 2,
+      sortable: true,
+    },
+    {
+      field: 'geo_region',
+      headerName: 'Region',
+      flex: 1,
+      sortable: true,
+    },
+    {
+      field: 'geo_latitude',
+      headerName: 'GEO latitude',
+      flex: 0.5,
+      type: 'number',
+      sortable: true,
+    },
+    {
+      field: 'geo_longitude',
+      headerName: 'GEO longitude',
+      flex: 0.5,
+      type: 'number',
+      sortable: true,
+    },
+  ];
+
+  return (
+      <Box className={classes.table}>
+        <DataGrid
+            columns={columns}
+            rows={data}
+            pageSize={15}
+            disableColumnMenu
+            disableDensitySelector
+            loading={isLoading}
+            components={{Toolbar: GridToolbar}}
+            checkboxSelection/>
+      </Box>
+  );
+}
+
+export default CloudsDataGrid;
+
+
+
--- src/pages/clouds/index.js
+++ src/pages/clouds/index.js
@@ -1,27 +1,40 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import DefaultLayout from '../../layouts/default';
 import Typography from '@material-ui/core/Typography';
 import {GetClouds} from '../../services/api';
+import CloudsDataGrid from '../../components/clouds-data-grid';
 
 function CloudsPage() {
+  const [clouds, setClouds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     GetClouds()
         .then(response => {
-          console.table(response.data?.clouds);
+          return response.data?.clouds.map((item, index) => ({
+            ...item, id: index,
+          }));
+        })
+        .then(response => {
+          console.table(response);
+          setClouds(response);
+          setIsLoading(false);
         })
         .catch(error => {
           //TODO: Use material-ui for show errors
+          console.log(error.message);
           alert(error.message);
+          setIsLoading(false);
         });
-    console.log();
-  });
+  }, []);
 
   return (
       <DefaultLayout>
         <Typography variant="h5" gutterBottom>
-          Cloud page
+          Clouds list
         </Typography>
+        <CloudsDataGrid data={clouds} isLoading={isLoading}/>
       </DefaultLayout>
   );
 }
